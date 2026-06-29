0d640e1046494c82001d070bc764ed13b295a22a
dasbywebdesign <dasbywebdesign@gmail.com>
2026-06-28 23:34:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+function App() {
+  return React.createElement('div', 
+    {style: {padding: 40, fontFamily: 'Arial'}},
+    React.createElement('h1', {style: {color: '#0D1B3E'}}, '⚡ Creator Agency OS'),
+    React.createElement('p', null, 'App is loading correctly. Full app coming next.')
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
